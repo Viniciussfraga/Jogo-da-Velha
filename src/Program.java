25f3656013d4242b34f24f4551744c7fbb9fb8db
Vinicius <viniciusfrg@outlook.com.br>
2022-09-02 19:48:09 -0300

    Dia 02: Immplementação da impressão do tabuleiro e melhorias na estrutura do código

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,6 @@
-import java.util.Arrays;
 import java.util.Scanner;
+
+import tabuleiro.Tabuleiro;
 
 public class Program {
 
@@ -7,9 +8,10 @@ public class Program {
 
 		System.out.println("Para iniciar o jogo tecle 'p' e para sair do jogo, tecle 'e'. ");
 		Scanner input = new Scanner(System.in);
-		char[][] mat = new char[3][3];
 		char tecla = input.next().toLowerCase().charAt(0);
-		StringBuilder sb = new StringBuilder( 'g');
+		Tabuleiro tabuleiro = new Tabuleiro();
+		tabuleiro.iniciarTab();
+		int jogador = 0;
 
 		if (tecla == 'p') {
 			System.out.println("Jogador um é representando por 'x' e jogador dois é representado por 'o'. ");
@@ -21,73 +23,24 @@ public class Program {
 			System.out.println("	20 | 21 | 22 ");
 		} else if (tecla == 'e') {
 			System.exit(tecla);
-
 		}
 
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				System.out.println("Quem joga agora? jogador 1 ou 2? ");
-				int jogador = input.nextInt();
+				jogador = input.nextInt();
 
-				// posição x
 				if (jogador == 1) {
-					System.out.println("Faça sua jogada, escolha uma posição (x): ");
+					System.out.println("Faça sua jogada jogador 1! Escolha uma posição (x): ");
 					int posicao = input.nextInt();
-					if (posicao >= 00 && posicao <= 02 || posicao >= 10 && posicao <= 12 || posicao >= 20 && posicao <= 22) {
-						if (posicao == 00) {
-							mat[0][0] = 'x';
-						} else if (posicao == 01) {
-							mat[0][1] = 'x';
-						} else if (posicao == 02) {
-							mat[0][2] = 'x';
-						} else if (posicao == 10) {
-							mat[1][0] = 'x';
-						} else if (posicao == 11) {
-							mat[1][1] = 'x';
-						} else if (posicao == 12) {
-							mat[1][2] = 'x';
-						} else if (posicao == 20) {
-							mat[2][0] = 'x';
-						} else if (posicao == 21) {
-							mat[2][1] = 'x';
-						} else if (posicao == 22) {
-							mat[2][2] = 'x';
-						}
-					} else {
-						System.out.println("Posição invalida!");
-					}
-
+					tabuleiro.mTabuleiro(posicao, jogador);
 				}
-				// posição o
 				if (jogador == 2) {
-					System.out.println("Faça sua jogada, escolha uma posição (o): ");
+					System.out.println("Faça sua jogada jogador 2! Escolha uma posição (o): ");
 					int posicao = input.nextInt();
-					if (posicao >= 00 && posicao <= 02 || posicao >= 10 && posicao <= 12 || posicao >= 20 && posicao <= 22) {
-						if (posicao == 00) {
-							mat[0][0] = 'o';
-						} else if (posicao == 01) {
-							mat[0][1] = 'o';
-						} else if (posicao == 02) {
-							mat[0][2] = 'o';
-						} else if (posicao == 10) {
-							mat[1][0] = 'o';
-						} else if (posicao == 11) {
-							mat[1][1] = 'o';
-						} else if (posicao == 12) {
-							mat[1][2] = 'o';
-						} else if (posicao == 20) {
-							mat[2][0] = 'o';
-						} else if (posicao == 21) {
-							mat[2][1] = 'o';
-						} else if (posicao == 22) {
-							mat[2][2] = 'o';
-						}
-					} else {
-						System.out.println("Posição invalida!");
-					}
+					tabuleiro.mTabuleiro(posicao, jogador);
 				}
-				sb.append(mat);
-				System.out.println(mat.toString());
+				tabuleiro.imprimeTab();
 			}
 		}
 		input.close();
--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -2,11 +2,84 @@ package tabuleiro;
 
 public class Tabuleiro {
 
-	private int linhas, colunas;
-	private char [][] tabuleiro = new char[3][3];
-	
+	private char[][] tabuleiro = new char[3][3];
+
 	public Tabuleiro() {
-		
 	}
-	
+
+	public char[][] mTabuleiro(int posicao, int jogador) {
+
+		// posição x
+		if (jogador == 1) {
+			if (posicao >= 00 && posicao <= 02 || posicao >= 10 && posicao <= 12 || posicao >= 20 && posicao <= 22) {
+				if (posicao == 00) {
+					tabuleiro[0][0] = 'x';
+				} else if (posicao == 01) {
+					tabuleiro[0][1] = 'x';
+				} else if (posicao == 02) {
+					tabuleiro[0][2] = 'x';
+				} else if (posicao == 10) {
+					tabuleiro[1][0] = 'x';
+				} else if (posicao == 11) {
+					tabuleiro[1][1] = 'x';
+				} else if (posicao == 12) {
+					tabuleiro[1][2] = 'x';
+				} else if (posicao == 20) {
+					tabuleiro[2][0] = 'x';
+				} else if (posicao == 21) {
+					tabuleiro[2][1] = 'x';
+				} else if (posicao == 22) {
+					tabuleiro[2][2] = 'x';
+				}
+			}
+		} // posição o
+		else if (jogador == 2) {
+			if (posicao >= 00 && posicao <= 02 || posicao >= 10 && posicao <= 12 || posicao >= 20 && posicao <= 22) {
+				if (posicao == 00) {
+					tabuleiro[0][0] = 'o';
+				} else if (posicao == 01) {
+					tabuleiro[0][1] = 'o';
+				} else if (posicao == 02) {
+					tabuleiro[0][2] = 'o';
+				} else if (posicao == 10) {
+					tabuleiro[1][0] = 'o';
+				} else if (posicao == 11) {
+					tabuleiro[1][1] = 'o';
+				} else if (posicao == 12) {
+					tabuleiro[1][2] = 'o';
+				} else if (posicao == 20) {
+					tabuleiro[2][0] = 'o';
+				} else if (posicao == 21) {
+					tabuleiro[2][1] = 'o';
+				} else if (posicao == 22) {
+					tabuleiro[2][2] = 'o';
+				}
+			}
+		} else {
+			System.out.println("Jogador inválido");
+		}
+		return tabuleiro;
+	}
+
+	public void iniciarTab() {
+		for (int i = 0; i < tabuleiro.length; i++) {
+			for (int j = 0; j < tabuleiro[i].length; j++) {
+				tabuleiro[i][j] = '-';
+			}
+		}
+	}
+
+	public void imprimeTab() {
+		for (int i = 0; i < tabuleiro.length; i++) {
+			for (int j = 0; j < tabuleiro[i].length; j++) {
+				if (j <= 1) {
+					System.out.print(tabuleiro[i][j] + " | ");
+				} else {
+					System.out.print(tabuleiro[i][j]);
+				}
+			}
+			System.out.println();
+		}
+	}
+
 }
